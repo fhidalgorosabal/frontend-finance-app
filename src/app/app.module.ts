3cff72f2362d45d355e6af71aeaddd237730b97b
Fernando Hidalgo Rosabal <fhidalgorosabal@gmail.com>
2023-02-12 13:27:30 -0500

    Diseño de interfaz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { StyleClassModule } from 'primeng/styleclass';
+import { MenubarModule } from 'primeng/menubar';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { TabViewModule } from 'primeng/tabview';
+import { BadgeModule } from 'primeng/badge';
+import { PanelModule } from 'primeng/panel';
+import { ToastModule } from 'primeng/toast';
+import { MenuModule } from 'primeng/menu';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +22,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    StyleClassModule,
+    MenubarModule,
+    InputTextModule,
+    ButtonModule,
+    TabViewModule,
+    BadgeModule,
+    PanelModule,
+    ToastModule,
+    MenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
